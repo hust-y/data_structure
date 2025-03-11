b377b9c0c401236c8c3971649a5ee4ffed874cee
zezczz <2327156313@qq.com>
2025-03-11 11:18:06 +0800

    the code after checked

--- joseph1/fun .c
+++ joseph1/fun .c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-LIST *initList(){
+LIST *initList()
+{
     LIST *l = (LIST *)malloc(sizeof(LIST));
-    if (l == NULL){
+    if (l == NULL)
+    {
         printf("Memory allocation failed\n");
         exit(1);
     }
@@ -14,18 +15,21 @@ LIST *initList(){
     return l;
 }
 
-void insert(LIST *l, int element){
+void insert(LIST *l, int element)
+{
     LIST *newNode = (LIST *)malloc(sizeof(LIST));
-    if (newNode == NULL){
+    if (newNode == NULL)
+    {
         printf("Memory allocation failed\n");
         exit(1);
     }
-    if (l->next == NULL){
+    if (l->next == NULL)
+    {
         l->next = newNode;
     }
     newNode->element = element;
     newNode->next = l->next;
-    if(newNode->next == NULL)
+    if (newNode->next == NULL)
     {
         newNode->next = l->next;
     }
@@ -33,13 +37,16 @@ void insert(LIST *l, int element){
     l->element++;
 }
 
-int deleteList(LIST *l, int index){
-    if (index < 0 || index >= l->element){
+int deleteList(LIST *l, int index)
+{
+    if (index < 0 || index >= l->element)
+    {
         printf("Invalid index\n");
         return -1;
     }
     LIST *temp = l;
-    for (int i = 0; i < index; i++){
+    for (int i = 0; i < index; i++)
+    {
         temp = temp->next;
     }
     LIST *delNode = temp->next;
@@ -50,13 +57,16 @@ int deleteList(LIST *l, int index){
     return element;
 }
 
-int isEmpty(LIST *l){
+int isEmpty(LIST *l)
+{
     return l->element == 0;
 }
 
-DELIST *initDEList(){
+DELIST *initDEList()
+{
     DELIST *l = (DELIST *)malloc(sizeof(DELIST));
-    if (l == NULL){
+    if (l == NULL)
+    {
         printf("Memory allocation failed\n");
         exit(1);
     }
@@ -64,56 +74,53 @@ DELIST *initDEList(){
     l->prev = NULL;
     l->next = NULL;
     return l;
-}//with head pointers
+} // with head pointers
 
 void insertDE(DELIST *l, int element)
 {
     DELIST *newNode = (DELIST *)malloc(sizeof(DELIST));
-    if (newNode == NULL){
+    if (newNode == NULL)
+    {
         printf("Memory allocation failed\n");
         exit(1);
     }
-    if(l->next == NULL)
+    if (l->next == NULL)
     {
         l->prev = newNode;
-        newNode->next = newNode;
-    }else
+    }
+    else
     {
         newNode->next = l->next;
-        l->next->prev = newNode;
+        l->next->prev = newNode;    
     }
+    l->prev->next = newNode;
     newNode->element = element;
     newNode->prev = l->prev;
     l->next = newNode;
     l->element++;
 }
-//insert at the head of the list
+// insert at the head of the list
 
 int deleteDE(DELIST *l, int index)
 {
     DELIST *temp = l;
-    if(index < 0)
+    DELIST *delNode;
+    if (index < 0)
     {
-        while(index++)
+        while (++index)
         {
             temp = temp->prev;
         }
-        // DELIST *delNode = temp->prev;
-        // temp->prev = delNode->prev;
-        // delNode->prev->next = temp;
-        // int element = delNode->element;
-        // free(delNode);
-        // l->element--;
-        // return element;
-    }else
+        delNode = temp->prev;
+    }
+    else
     {
-        for (int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++)
+        {
             temp = temp->next;
         }
+        delNode = temp->next;
     }
-    
-    
-    DELIST *delNode = temp->next;
     int element = delNode->element;
     delNode->prev->next = delNode->next;
     delNode->next->prev = delNode->prev;
@@ -123,10 +130,10 @@ int deleteDE(DELIST *l, int index)
     l->element--;
     return element;
 }
-//delete the element at the given index
+// delete the element at the given index
 
 int isEmptyDE(DELIST *l)
 {
     return l->element == 0;
 }
-//check if the list is empty:0 for empty, 1 for not empty
+// check if the list is empty:0 for empty, 1 for not empty
--- joseph1/main.c
+++ joseph1/main.c
@@ -3,9 +3,9 @@
 #include <time.h>
 #include "func.h"
 
-#define N 10
-#define M 5
-#define T 1
+#define N 1000
+#define M 500
+#define T 100
 
 int *solution_with_list(int n, int m);
 
@@ -13,43 +13,50 @@ int *solution_with_delist(int n, int m);
 
 int *solution_with_sequence(int n, int m);
 
+int *solution_with_DEsequence(int n, int m);
+
 int main()
 {
     srand(time(NULL));
-    int n , m , t = T;
-    int *ans1 = (int*)malloc(n * sizeof(int));
-    int *ans2 = (int*)malloc(n * sizeof(int));
-    int *ans3 = (int*)malloc(n * sizeof(int));
-    while(t--)
+    int n, m, t = 1;
+    int *ans1 = (int *)malloc(n * sizeof(int));
+    int *ans2 = (int *)malloc(n * sizeof(int));
+    int *ans3 = (int *)malloc(n * sizeof(int));
+    int *ans4 = (int *)malloc(n * sizeof(int));
+    while (t--)
     {
-        n = 15, m = 1;
+        n = 10, m = 5;
         ans1 = solution_with_list(n, m);
         ans2 = solution_with_delist(n, m);
         ans3 = solution_with_sequence(n, m);
-    }
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d %d %d\n" , ans1[i] , ans2[i] , ans3[i]);
-        if (ans1[i] != ans2[i])
+        ans4 = solution_with_DEsequence(n, m);
+        for (int i = 0; i < n; i++)
         {
-            printf("Error!\n");
-            return 0;
+            printf("%d %d %d %d\n", ans1[i], ans2[i], ans3[i],ans4[i]);
+            if (ans1[i] != ans2[i])
+            {
+                printf("Error!\n");
+                return 0;
+            }
         }
     }
+    
     printf("It is OK!\n");
     // check the correctness of the solution
+
     float begin, end;
     t = T;
     begin = clock();
-    while(t--)
+    while (t--)
     {
         int n = N, m = rand() % M + 1;
         solution_with_list(n, m);
     }
     end = clock();
+    t = T;
     printf("Time taken by solution_with_list() is %f seconds\n", (end - begin) / CLOCKS_PER_SEC);
     begin = clock();
-    while(t--)
+    while (t--)
     {
         int n = N, m = rand() % M + 1;
         solution_with_delist(n, m);
@@ -60,16 +67,15 @@ int main()
 
 int *solution_with_list(int n, int m)
 {
-    int *arr = (int*)malloc(n * sizeof(int));
+    int *arr = (int *)malloc(n * sizeof(int));
     LIST *l = initList();
-    int i , x = 0;
-    for( i = n; i >= 1 ; i-- )
+    int i, x = 0;
+    for (i = n; i >= 1; i--)
     {
         insert(l, i);
     }
-    for( i = 0; i < n; i++ )
+    for (i = 0; i < n; i++)
     {
-        // printf("%d " , m);
         x += m - 1;
         x %= n - i;
         m = deleteList(l, x);
@@ -81,49 +87,86 @@ int *solution_with_list(int n, int m)
 
 int *solution_with_delist(int n, int m)
 {
-    int *arr = (int*)malloc(n * sizeof(int));
+    int *arr = (int *)malloc(n * sizeof(int));
     DELIST *l = initDEList();
-    int i,x;
-    for( i = n; i >= 1 ; i-- )
+    int i, x;
+    for (i = n; i >= 1; i--)
     {
         insertDE(l, i);
     }
-    for( i = 0; i < n; i++ )
+    for (i = 0; i < n; i++)
     {
-        // printf("%d\n" , x);
-        // printf("%d " , m);
         int x = (m - 1) % (n - i);
         if (x > l->element / 2)
         {
-            x = x - l->element - 1;
+            x = x - l->element;
         }
 
         m = deleteDE(l, x);
-        
+
         *(arr + i) = m;
     }
     return arr;
 }
 
-int* solution_with_sequence(int n, int m) {
-    int *arr = (int*)malloc(n * sizeof(int));
-    int *circle = (int*)malloc(n * sizeof(int));
+int *solution_with_sequence(int n, int m)
+{
+    int *arr = (int *)malloc(n * sizeof(int));
+    int *circle = (int *)malloc(n * sizeof(int));
     int i, pos = 0;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         circle[i] = i + 1;
     }
 
     int remaining = n;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         pos = (pos + m - 1) % remaining;
         arr[i] = circle[pos];
         m = arr[i];
-        for (int j = pos; j < remaining - 1; j++) {
+        for (int j = pos; j < remaining - 1; j++)
+        {
             circle[j] = circle[j + 1];
         }
         remaining--;
     }
 
     free(circle);
+    return arr;
+}
+
+int *solution_with_DEsequence(int n, int m)
+{
+    int *arr = (int *)malloc(n * sizeof(int));
+    int *circle = (int *)malloc(n * sizeof(int));
+    int first = 0 , last = n - 1;
+    int i, pos = 0;
+    for (i = 0; i < n; i++)
+    {
+        circle[i] = i + 1;
+    }
+    for (i = 0; i < n; i++)
+    {
+        pos = (pos - first + m - 1) % (last - first + 1) + first;
+        arr[i] = circle[pos];
+        m = arr[i];
+        if(pos > (first + last)/2)
+        {
+            for (int j = pos; j < last; j++)
+            {
+                circle[j] = circle[j + 1];
+            }
+            last--;
+        }else
+        {
+            for (int j = pos; j > first; j--)
+            {
+                circle[j] = circle[j - 1];
+            }
+            pos++;
+            first++;
+        }
+    }
     return arr;
 }
