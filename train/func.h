c1ef21c385655319551022dd11073ec1803412f8
zezczz <2327156313@qq.com>
2025-03-31 18:53:01 +0800

    upload func.h

--- train/func.h
+++ train/func.h
@@ -11,6 +11,35 @@ int ans[100];
 
 void print_operation(int n);
 
+typedef struct Stack {
+    int top;
+    int elements[100];
+}stack;
 
+void create_stack(stack* s);
+
+void push(stack* s, int value);
+
+int top(stack* s);
+
+void pop(stack* s);
+
+typedef struct Destack {
+    int top1;
+    int top2;
+    int elements[100];
+    int n;
+}destack;
+//left is zhan, right is ans and the direction is from right to left
+
+void create_destack(destack* d);
+
+void pushleft(destack* d, int value);
+
+void pushright(destack* d, int value);
+
+int topleft(destack* d);
+
+int printans(destack* d);
 
 #endif
