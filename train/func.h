8a40b46a10dbdc6a3c3abd8d49c4c7375f0cf818
zezczz <2327156313@qq.com>
2025-03-31 18:34:50 +0800

    create a dictionary

---
+++ train/func.h
@@ -0,0 +1,16 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+typedef struct Node {
+    char operator[20];
+    int id;
+}node;
+
+node operation[100];
+int ans[100];
+
+void print_operation(int n);
+
+
+
+#endif
---
+++ train/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "func.h"
+
+int main()
+{
+    int n;
+    
+}
