5a9dade2870060fa714467ae12246757d101bfab
l-iang <3266456420@qq.com>
2025-04-04 21:56:17 +0800

    Complish my work4:which is use array and recurse.

--- train/work.h
+++ train/work.h
@@ -15,4 +15,17 @@ void work3(int n);
 
 void work3_dfs(int pos , int id , int n , destack *s);
 
+void work4(int n);
+#define MAX 1000
+int cattelan[MAX];
+
+void set_cattelan(int n);
+void display_ans(int n, int *ans, int nth_cattelan);
+void get_ith_ans(int i, int n,  int *ans, int *temp);
+void set_one(int i, int *ans, int n);
+void set_pre(int i, int *ans, int *temp, int n);
+void set_end(int i, int *ans, int *temp, int n);
+
+void print_oneline(int *a, int n);
+
 #endif
--- train/work4.c
+++ train/work4.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
 //#include "work.h"
 #include "func.h"
+#include "work.h"
 
-#define MAX 1000
-int cattelan[MAX];
-
-void set_cattelan(int n);
-void display_ans(int n, int *ans, int nth_cattelan);
-void get_ith_ans(int i, int n,  int *ans, int *temp);
-void set_one(int i, int *ans);
-void set_pre(int i, int *ans, int *temp);
-void set_end(int i, int *ans, int *temp);
-
-//void work4(int n)
-int main(void)
+void work4(int n)
 {
-    int n = 4; // Example value for n, you can change it as needed
+    int n = 3; // Example value for n, you can change it as needed
     set_cattelan(n);
 
     int ans[cattelan[n] * n];
     int temp[cattelan[n] * n];
-    //set_one(n, ans);
+    get_ith_ans(n, n, ans, temp);
     display_ans(n, ans, cattelan[n]);
-    printf("Cattelan numbers:\n");
-    /*for(int i = 0; i <= n; i++)
-    {
-        printf("%d ", cattelan[i]);
-    }
-    printf("\n");*/
+    return 0;
 }
 
 void set_cattelan(int n)
@@ -62,32 +47,91 @@ void get_ith_ans(int i, int n,  int *ans, int *temp)
         return;
     }
     get_ith_ans(i - 1, n, temp, ans);
-    set_one(i, ans);//Set the split one num in the ans array
-    set_pre(i, ans, temp);//Set the pre num in the ans array
-    set_end(i, ans, temp);//Set the end num in the ans array
+    printf("i = %d\n", i);
+    set_one(i, ans, n);//Set the split one num in the ans array
+    set_pre(i, ans, temp, n);//Set the pre num in the ans array
+    set_end(i, ans, temp, n);//Set the end num in the ans array
     return;
 }
-void set_one(int i, int *ans)
+void set_one(int i, int *ans, int n)
 {
     int now = 0;
     for(int j = 0; j <= i; j++)
     {
         for(int k = now; k - now < cattelan[j] * cattelan[i - j - 1]; k++)
         {
-            ans[k * i + j] = 1;
+            ans[k * n + j] = 1;
         }
         now += cattelan[j] * cattelan[i - j - 1];
     }
     return;
 }
-void set_pre(int i, int *ans, int *temp)
+void set_pre(int i, int *ans, int *temp, int n)
 {
     int now_for_ans = cattelan[i - 1];
     int now_for_temp = cattelan[i - 2];
-    for(int j = 1; j < i - 1; j ++)
+    for(int j = 1; j < i - 1; j ++)//set value to i - 1 and the last, which is i, is seted alone
     {
-        
+        for(int k = 0; k < cattelan[j]; k ++)
+        {
+            for(int l = 0; l < cattelan[i - j - 1]; l++)
+            {
+                copy_add_array(ans + (now_for_ans + k * cattelan[i - j - 1] + l) * n, temp + (now_for_temp + k * cattelan[i - j - 2]) * n, j, 0);
+            }
+        }
+        now_for_ans += cattelan[j] * cattelan[i - j - 1];
+        now_for_temp += cattelan[j] * cattelan[i - j - 2]; 
     }
+    for(int k = 0; k < cattelan[i - 1]; k ++)
+    {
+        copy_add_array(ans + (now_for_ans + k) * n, temp + k * n, i - 1, 1);
+    }
+    return;
 }
-void set_end(int i, int *ans, int *temp)
-{}
+void set_end(int i, int *ans, int *temp, int n)
+{
+    int now_for_ans = 0;
+    int now_for_temp = 0;
+    for(int j = 0; j < cattelan[i - 1]; j ++)
+    {
+        copy_add_array(ans + j * n + 1, temp + j * n, i - 1, 1);//Set the first block in the ans array. 
+    }
+    for(int j = 1; j < i - 1; j ++)//Set value from the second block of the array, and leave the last alone, which doesn't need to be set.
+    {
+        for(int k = 0; k < cattelan[i - j - 2]; k ++)
+        {
+            for(int l = 0; l < cattelan[j]; l++)
+            {
+                copy_add_array(ans + (now_for_ans + l * cattelan[i - j - 1] + k) * n + j + 1, \
+                temp + (now_for_temp + k) * cattelan[i - j - 2] + j, i - 1 - j, 1);
+            }
+        }
+        now_for_ans += cattelan[j] * cattelan[i - j - 1] ;
+        now_for_temp += cattelan[j] * cattelan[i - j - 2];
+    }
+    return;
+}
+
+//My array functions
+void copy_add_array(int *a, int *b, int n, int m)
+{
+    printf("%d ", n);
+    printf("add num %d to array:", m);
+    printf("copy_add_array:");
+    print_oneline(b, n);
+    for(int i = 0 ; i < n ; i++ )
+    {
+        a[i] = b[i] + m;
+    }
+    return;
+}
+
+void print_oneline(int *a, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return;
+}
