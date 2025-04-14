db053df98d74ab635578a7cb24dd38533de118a2
zezczz <2327156313@qq.com>
2025-04-14 19:01:46 +0800

    create files

---
+++ hfmTree/func.h
@@ -0,0 +1,36 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+
+
+typedef struct Node {
+    char name;//0 for not leaf node
+    struct node *left;
+    struct node *right;
+}node;
+typedef struct Listelement{
+    int value;
+    node *Location;
+}listelement;
+
+typedef struct List{
+    listelement *data;
+    struct List *next;
+}list;
+
+void ini(node *root , char *dictionary);
+// to generate the dictionary
+
+void enc(const char *filename1, char *filename2 , char *dictionary);
+//encode the 1 to 2;
+
+void dec(const char *filename1, char *filename2 , char *dictionary);
+//decode the 1 to 2;
+
+void pri(const char *filename);
+//print the dictionary;
+
+void print_tree(node *root);
+//print the tree;
+
+#endif FUNC_H
